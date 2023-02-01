b5c86e8e0e17c99a624c6ed65f337bc4c0721729
asaraf42 <123632931+asaraf42@users.noreply.github.com>
2023-02-02 00:16:12 +0530

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,24 @@
+package com.app.account;
+
+public abstract class Account {
+	
+	int accno;
+	double accbal;
+	
+	public Account(int accno,double accbal)
+	{
+		this.accno=accno;
+		this.accbal=accbal;
+	}
+	
+	 public  void deposit(double amt) {
+		 
+	 }
+
+	 public  void withdraw(double amt) {
+		 
+	 }
+	
+}
+
+
